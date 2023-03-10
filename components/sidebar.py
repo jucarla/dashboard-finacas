01bda6c52e4394bddd7621aaeda1e9350c213ceb
jucarla <89020112+jucarla@users.noreply.github.com>
2023-03-10 00:17:49 -0300

    first commit

---
+++ components/sidebar.py
@@ -0,0 +1,56 @@
+import os
+import dash
+from dash import html, dcc
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+from app import app
+
+from datetime import datetime, date
+import plotly.express as px
+import numpy as np
+import pandas as pd
+
+
+
+
+
+
+# ========= Layout ========= #
+layout = dbc.Col([
+                html.H1("MyBudget", className="text-primary"),
+                html.P("By ASIMOV", className="text-info"),
+                html.Hr(),
+    # Seção PERFIL ------------------
+                dbc.Button(id='botao_avatar',
+                    children=[html.Img(src='/assets/img_hom.png', id='avatar_change', alt='Avatar', className='perfil_avatar')
+                    ], style ={'background-color':'transparent', 'border-color':'transparent'}),
+    
+    # Seção NOVO --------------------
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Button(color='success', id='open-novo-receita',
+                                children=['+ Receita'])
+                    ], width = 6),
+                    dbc.Col([
+                        dbc.Button(color='danger', id='open-novo-despesa',
+                                children=['- Despesa'])
+                    ], width = 6)
+                ]),
+    # Seção NAV ------------------
+    # href -> saber que pagina o programa irá ao clicar nos botões
+    # pills -> botão em torno do menu quando ele é selecionado
+                html.Hr(),
+                dbc.Nav(
+                    [
+                        dbc.NavLink("Dashboard", href="/dashboards", active="exact"),
+                        dbc.NavLink("Extratos", href="/extratos", active = "exact"),
+                    ], vertical=True, pills=True, id='nav_buttons', style={'margin-bottom': "50px"}),
+                
+], id="sidebar-completa")
+
+
+
+
+
+# =========  Callbacks  =========== #
+# Pop-up receita
